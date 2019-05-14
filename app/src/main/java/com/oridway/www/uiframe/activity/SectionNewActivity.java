a19e996c04acdc4e415203ac662374b069a34a7d
Camgrolyph <qazxdr15937@163.com>
2019-05-14 18:03:44 +0800

    添加注释

--- app/src/main/java/com/oridway/www/uiframe/activity/SectionNewActivity.java
+++ app/src/main/java/com/oridway/www/uiframe/activity/SectionNewActivity.java
@@ -97,11 +97,14 @@ public class SectionNewActivity extends AppCompatActivity implements View.OnClic
         sectionManager.setOnClickListener(this);
         selectAttach.setOnClickListener(this);
 
+        //点击附件列表条目的删除按钮，删除对应附件
         mAdapter.setmCallback((view, position) -> {
             mAttachList.remove(position);
             mAdapter.notifyDataSetChanged();
         });
 
+
+        //点击附件列表弹出打开方式
         mListView.setOnItemClickListener((parent, view, position, id) -> {
 
             ClsAttachMent clsAttachMent = mAttachList.get(position);
@@ -127,8 +130,10 @@ public class SectionNewActivity extends AppCompatActivity implements View.OnClic
             submit();
         }
         if (v.getId() == R.id.section_new_logo) {
+            //打开手机原生的浏览器，并且选取内容
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.addCategory(Intent.CATEGORY_OPENABLE);
+            //文件类型为图片
             intent.setType("image/*");
             startActivityForResult(intent, 16352);
         }
@@ -139,6 +144,7 @@ public class SectionNewActivity extends AppCompatActivity implements View.OnClic
 
         if (v.getId() == R.id.tv_upload_attach) {
 
+            //上传的附件数量不能超过4个
             if (mAttachList.size() < 4) {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -161,16 +167,22 @@ public class SectionNewActivity extends AppCompatActivity implements View.OnClic
     }
 
     @Override
+    //requestCode要对应上，resultCode都为默认值RESULT_OK
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //选择图片完成之后使用glide加载到控件上，此处有时需要把图片上传给后台
+        //提交数据的时候传图片在后台的路径
         if (requestCode == 16352 && resultCode == RESULT_OK) {
             Glide.with(mContext).load(data.getData()).into(sectionLogo);
         }
 
+        //打开选择用户的页面，根据传的参数不同页面也不同，默认是单选页面
         if (requestCode == 12345 && resultCode == RESULT_OK) {
             ClsNormalUser user = data.getParcelableExtra("user");
             sectionManager.setText(user.getCName());
         }
 
+        //遍立已经上传的附件列表，如果已经存在就弹出提示
         if (requestCode == 12367 && resultCode == RESULT_OK) {
             String uri = data.getData().toString();
 
@@ -194,6 +206,7 @@ public class SectionNewActivity extends AppCompatActivity implements View.OnClic
     }
 
     private void addAttach(Intent data) {
+        //这里使用第三方库ucrop的getPath方法，也可以自己实现uri转换为path
         File file = new File(getPath(mContext, data.getData()));
 
         ClsAttachMent clsAttachMent = new ClsAttachMent();
@@ -204,6 +217,7 @@ public class SectionNewActivity extends AppCompatActivity implements View.OnClic
         clsAttachMent.setFilename(name);
         clsAttachMent.setUri(data.getData().toString());
 
+        //这里需要调用上传接口
         uploadFile(file.getPath());
 
         mAttachList.add(clsAttachMent);
